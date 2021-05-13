d7d47a7b39a0c71022103d21f459b531df569f1b
marco <marco.apolinario.l@gmail.com>
2021-05-13 00:01:46 -0400

    visualization poisson generator

---
+++ experiments/poisson_generator.py
@@ -0,0 +1,33 @@
+import torch
+from torchvision import datasets, transforms
+import numpy as np
+import matplotlib.pyplot as plt
+
+transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5),(0.5))])
+
+trainset = datasets.MNIST('./MNIST_data/', download=True, train=True, transform=transform)
+
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=False)
+
+x_input, labels = next(iter(trainloader))
+
+x_input = x_input[1, 0, :, :]
+out_1 = torch.zeros_like(x_input)
+out_2 = torch.zeros_like(x_input)
+
+y = np.zeros(64)
+for k in range(64):
+    plt.subplot(131)
+    plt.imshow(x_input)
+    plt.subplot(132)
+    out_2 = torch.mul(torch.le(torch.rand_like(x_input), torch.abs(x_input) * 1.0).float(), torch.sign(x_input))
+    plt.imshow(out_2)
+    plt.subplot(133)
+    plt.plot(y[:k], "b-*")
+    plt.xlim([0, 64])
+    plt.ylim([-1, 1])
+    plt.title(k)
+    plt.show()
+
+    plt.pause(0.1)
+    out_1 = out_2
