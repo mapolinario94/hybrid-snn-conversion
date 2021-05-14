e06fba2d53003dd8210cfe2417921fcb458dbd8c
mapolina <marco.apolinario.l@gmail.com>
2021-05-14 18:29:10 -0400

    comment

--- experiments/poisson_generator.py
+++ experiments/poisson_generator.py
@@ -3,7 +3,11 @@ from torchvision import datasets, transforms
 import numpy as np
 import matplotlib.pyplot as plt
 
-transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5),(0.5))])
+# A random number is generated at every time step for each pixel in the input image.
+# If the random number is less than the normalized pixel value an output spike is generated
+# out_2 = torch.mul(torch.le(torch.rand_like(x_input), torch.abs(x_input) * 1.0).float(), torch.sign(x_input))
+
+transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0),(0.5))])
 
 trainset = datasets.MNIST('./MNIST_data/', download=True, train=True, transform=transform)
 
